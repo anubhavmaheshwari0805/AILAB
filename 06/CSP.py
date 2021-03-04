781c8bd079e49916e8fd3321ce2c8ed586233172
Anubhav Maheshwari <55053867+anubhavmaheshwari0805@users.noreply.github.com>
2021-03-05 00:40:03 +0530

    Add files via upload

--- 06/CSP.py
+++ 06/CSP.py
@@ -1,58 +1,48 @@
 #!/usr/bin/env python
 
-def backtrack(x, enemy, domain, assigned) :
+def backtrack(soln, assigned, domain, enemy_list) :
     if -1 not in assigned :
-        return x
-    v=999
+        return soln
+    v=1000
+    w=0
     for i in range(len(domain)) :
-        if v>len(domain[i]) and assigned[i]!=1 :
-            v=i
-    order=[]
-    for i in domain[v] :
-        mini=1000
-        for j in enemy[v] :
-            temp=len(domain[j])
-            if i in domain[j] :
-                temp-=1
-            if mini>temp :
-                mini=temp
-        order.append((i,mini))
-    order=sorted(order,key=lambda x:x[1], reverse=True)
-    ordered=[i[0] for i in order]
-    for i in ordered :
-        newdomain=[[j for j in i] for i in domain]
-        x[v]=i
-        assigned[v]=1
-        newdomain[v]=[k for k in newdomain[v] if k==i]
-        temp=[]
-        for j in range(len(newdomain)) :
-            if j!=v and j in enemy[v] :
-                newdomain[j]=[k for k in newdomain[j] if k!=i]
-        res=backtrack(x, enemy, newdomain, assigned)
-        if res!=0:
+        if len(domain[i])<v and assigned[i]==-1 :
+            v=len(domain[i])
+            w=i
+    affected=[]
+    for i in domain[w] :
+        soln[w]=i
+        assigned[w]=1
+        for e in enemy_list[w] :
+            if i in domain[e] :
+                domain[e].remove(i)
+                affected.append(e)
+        res=backtrack(soln, assigned, domain, enemy_list)
+        if res!=0 :
             return res
-    x[v]=""
-    assigned[v]=-1
+        for a in afected :
+            domain[a].append(i)
+        affected=[]
     return 0
 
-n=8 #input("Enter the number of people : ")
-c=3 #input("Enter the number of tables : ")
-#l=[]
-#line=input("Enter elements of list L(people who should not sit together) till an empty newline character :\n").split()
-#while(line) :
-#    l.append((int(line[0]),int(line[1])))
-#    line=input().split()
-l=[(0,2),(0,3),(0,4),(1,4),(1,7),(2,3),(2,6),(3,4),(3,7),(4,7),(5,6)]
-enemy=[[] for i in range(n)]
-for i in l :
-    enemy[i[0]].append(i[1])
-    enemy[i[1]].append(i[0])
-x=["" for i in range(n)]
-assigned=[-1 for i in range(n)]
-domain=[[x for x in range(c)] for i in range(n)]
-res=backtrack(x, enemy, domain, assigned)
+people=8#int(input("Enter no. of peoples : "))
+tables=3#int(input("Enter no. of tables : "))
+constraints=[]
+#list=input("Enter the pair of peoples who can't sit together : ").split()
+#while list :
+#    constraints.append((int(list[0]),int(list[1])))
+#    list=input().split()
+constraints=[(0,2),(0,3),(0,4),(1,4),(1,7),(2,3),(2,6),(3,4),(3,7),(4,7),(5,6)]
+assigned=[-1 for i in range(people)]
+domain=[[x for x in range(tables)] for i in range(people)]
+soln=["" for i in range(people)]
+enemy_list=[[] for i in range(people)]
+for i in constraints :
+    enemy_list[i[0]].append(i[1])
+    enemy_list[i[1]].append(i[0])
+
+res=backtrack(soln,assigned,domain,enemy_list)
 if res==0 :
-    print('False')
-else:
-    for i in range(len(res)) :
-        print(i,' : ',res[i])
+    print("No solution")
+else :
+    print(res)
